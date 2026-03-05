d4f297f8378c3c4036ef44b2bc52d67601a39c7e
Priyail <nidhipandey8108@gmail.com>
2026-03-05 17:55:05 +0530

    update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0ba0849c690f5d0afa0add58c30e504e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1dcef533a606835c3f39f61f9c9722bd",
+"flutter_bootstrap.js": "2e728a66f2be2cd5a7cd11447be3810a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
