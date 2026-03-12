146a5b9a37038f48df6c374caab01e5b5b7ca57f
Priyail <nidhipandey8108@gmail.com>
2026-03-12 22:04:28 +0530

    update role in header

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0ba0849c690f5d0afa0add58c30e504e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "10d56e28670b515a45f9e439bb828686",
+"flutter_bootstrap.js": "def363c79f3e4c320f20d4a83db5991d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
