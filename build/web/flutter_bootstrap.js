312beff8be8530847fce8167573430072b360f69
Priyail <nidhipandey8108@gmail.com>
2026-03-12 22:01:13 +0530

    update role in header

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3362119623"
+    serviceWorkerVersion: "272331213"
   }
 });
